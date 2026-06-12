aeb70ecc4a0ee50884a23ea144dea296d0987d36
MARINA ESPÍNDOLA STEINBACH <marina_steinbach@estudante.sesisenai.org.br>
2026-06-12 11:09:57 -0300

    Adição do app.get

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+ 
+const app = express();
+ app.use(express.json());
+
+app.get('/', (req, res) => {
+res.json({ mensagem: 'Servidor funcionando!' });
+});
+ 
+app.listen(3000, () => {
+
+
+    console.log('Servidor rodando em http://localhost:3000');
+});
+
+
+const ingredientes = [
+{ id: 1, nome: 'Batata', quantidade_estoque: 25, minimo: 10, data_cadastro:"2026-06-12T10:56:22.123Z" },
+{ id: 2, nome: 'Carne Moída', quantidade_estoque: 35, minimo: 9, data_cadastro:"2026-06-12T10:56:25.233Z" },
+];
+ 
+app.get('/ingredientes', (req, res) => {
+res.json(ingredientes);
+});
+
+app.get('/ingredientes/:id', (req, res) => {
+const id = Number(req.params.id);
+const produto = ingredientes.find(p => p.id === id);
+ 
+if (!produto) {
+return res.status(404).json({ erro: 'Ingrediente não encontrado' });
+}
+ 
+res.json(produto);
+});
